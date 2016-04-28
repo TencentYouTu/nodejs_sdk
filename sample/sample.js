cfe9389d03c3b75940439c15f11c5bfd4210e1f3
TencentYouTu <youtu@tencent.com>
2016-04-28 10:44:39 +0800

    update
    update

--- sample/sample.js
+++ sample/sample.js
@@ -3,10 +3,10 @@ var conf  = tencentyoutuyun.conf;
 var youtu = tencentyoutuyun.youtu;
 
 // 设置开发者和应用信息, 请填写你在开放平台
-var appid = '1006920';
-var secretId = 'AKIDZ6O9LK4HfRgY1EAcJincpUu27iQdn99A';
-var secretKey = 'EU1TBKJfb0zRLFrpT7FuD0JMXBGIy2Yz';
-var userid = '1001';
+var appid = '****';
+var secretId = '****';
+var secretKey = '****';
+var userid = '****';
 
 conf.setAppInfo(appid, secretId, secretKey, userid, 0)
 
